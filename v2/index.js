9c538a0c52fd319aa17d5f21fbe64928ea720051
vyrnz <86507452+vyrnz@users.noreply.github.com>
2024-12-24 21:25:17 +0700

    Add files via upload

---
+++ v2/index.js
@@ -0,0 +1,7 @@
+function ToggleNav() {
+    if (document.getElementById("navMenu").style.display === "none") {
+        document.getElementById("navMenu").style.display = "block";
+    } else {
+        document.getElementById("navMenu").style.display = "none";
+    }
+}
