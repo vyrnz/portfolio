5d21fe47da7f96a72de346e453ab833b396b19df
vyrnz <86507452+vyrnz@users.noreply.github.com>
2024-12-26 21:23:54 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-const img = document.querySelector("#about.content img");
-
-img.addEventListener("click", () => {
-  const newImage = document.querySelector("images/Profile.jpeg");
-  img.src = newImage.src;
-});
-
-
