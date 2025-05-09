4cc07bfca4795d1322dbe42382b534e8fb91f598
vyrnz <86507452+vyrnz@users.noreply.github.com>
2025-05-09 15:50:04 +0700

    Delete v2 directory

--- v2/index.js
+++
@@ -1,7 +0,0 @@
-function ToggleNav() {
-    if (document.getElementById("navMenu").style.display === "none") {
-        document.getElementById("navMenu").style.display = "block";
-    } else {
-        document.getElementById("navMenu").style.display = "none";
-    }
-}
